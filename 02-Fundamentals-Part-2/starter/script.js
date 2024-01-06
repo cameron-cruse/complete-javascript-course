f75b6850958e1c384851e5b52d2e614ff515ced4
Cameron Cruse <132945596+cameron-cruse@users.noreply.github.com>
2024-01-05 22:56:06 -0600

    day one

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,67 @@
+'use strict';
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive :D');
+
+
+// // reserved words
+// // const interface = 'Audio';
+// // const private = 534;;
+// // const if = 23;
+
+
+// // functions
+
+// function logger() {
+//     console.log('My name is Jonas');
+// }
+
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//     // console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+
+// console.log(appleJuice);
+
+
+//function expression
+
+// const calcAge2 = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// const age2 = calcAge2(1991);
+// console.log(age2);
+
+// // arrow function 
+
+// const calcAge3 = birthYear => 2037 - birthYear;
+// const age3 = calcAge3(1991);
+// console.log(age3);
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//     const age = 2037 - birthYear;
+//     const retirement = 65 - age;
+//     return `${firstName} retires in ${retirement} years`;
+// }
+
+// console.log(yearsUntilRetirement(1991, 'Jonas'));
+// console.log(yearsUntilRetirement(1980, 'Bob'));
+
+// const friends = ['Michael', 'Steven', 'Peter'];
+// friends.push('Jay');
+// console.log(friends);
+
+
+//BUG
+//TODO
+//FIXME
+
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,57 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+}
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    //no input 
+    if (!guess) {
+        displayMessage('🚫 No Number!');
+        //player wins
+    } else if (guess === secretNumber) {
+        displayMessage('🎉 Correct Number!');
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        document.querySelector('.number').textContent = secretNumber;
+
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
+        //guess is wrong
+    } else if (guess != secretNumber) {
+        if (score > 1) {
+            displayMessage(guess > secretNumber ? '📈 Too High!' : '📉 Too Low!');
+            score -= 1;
+            document.querySelector('.score').textContent = score;
+        } else {
+            displayMessage('💥 You lost the game!');
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+});
+
+
+document.querySelector('.again').addEventListener('click', function () {
+    //reset score and update display
+    score = 20;
+    document.querySelector('.score').textContent = score;
+    //generate new number
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    //reset message
+    displayMessage('Start guessing...')
+    //reset guess value
+    document.querySelector('.guess').value = '';
+    //reset bg color and text width
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+
+    //reset question mark
+    document.querySelector('.number').textContent = '?';
+})
